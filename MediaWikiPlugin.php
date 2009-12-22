f64f6da87a17cd4840febe2cf4319ecd20e061ff
Brion <brion@stormcloud.(none)>
2009-12-22 10:13:14 -0800

    update for StatusNet & new plugin-loading/config include [[ and ]] in output so messages remain cut-n-pasteable fix encoding/escaping on urls

--- MediaWikiPlugin.php
+++ MediaWikiPlugin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Laconica, the distributed open-source microblogging tool
+ * StatusNet, the distributed open-source microblogging tool
  *
  * Plugin to parse MediaWiki-style freelinks into the Wiki of your choice.
  *
@@ -20,7 +20,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @category  Plugin
- * @package   Laconica
+ * @package   StatusNet
  * @author    Eugene Eric Kim <eekim@blueoxen.com>
  * @copyright 2009 Blue Oxen Associates
  * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License version 3.0
@@ -29,12 +29,11 @@
  * @see      Event
  */
 
-if (!defined('LACONICA')) {
+if (!defined('LACONICA') && !defined('STATUSNET')) {
     exit(1);
 }
 
 define('MEDIAWIKIPLUGIN_VERSION', '0.1');
-define('WIKI_BASE_URL', 'http://foo/wiki/');
 
 /**
  * Plugin to parse MediaWiki-style freelinks into the Wiki of your choice.
@@ -53,34 +52,32 @@ define('WIKI_BASE_URL', 'http://foo/wiki/');
 
 class MediaWikiPlugin extends Plugin
 {
-    var $base_url = null;
-
-    function __construct()
-    {
-        parent::__construct();
-    }
+    public $base_url = 'http://foo/wiki/';
 
     function onStartNoticeSave($notice)
     {
+        // @fixme this may cross link boundaries etc and be funky
         $notice->rendered = preg_replace_callback('/\[\[[^\]]+\]\]/',
-                                          "renderWikiLink",
-                                           $notice->rendered);
+                                          array($this, "renderWikiLink"),
+                                          $notice->rendered);
         return true;
     }
 
-    function userAgent()
+    function renderWikiLink($match)
     {
-        return 'MediaWikiPlugin/'.MEDIAWIKIPLUGIN_VERSION .
-          ' Laconica/' . LACONICA_VERSION;
+        $html = preg_replace('/^\[\[/', '', $match[0]);
+        $html = preg_replace('/\]\]$/', '', $html);
+
+        $page = html_entity_decode($html, ENT_COMPAT, 'utf-8');
+        $page = str_replace(' ', '_', $page);
+
+        $encoded = urlencode($page);
+        $encoded = str_replace('%2F', '/', $encoded);
+        $encoded = str_replace('%3A', ':', $encoded);
+
+        $url = $this->base_url . $encoded;
+        $encurl = htmlspecialchars($url);
+        return "[[<a href=\"$encurl\">$html</a>]]";
     }
 }
 
-function renderWikiLink($match)
-{
-    $match[0] = preg_replace('/^\[\[/', '', $match[0]);
-    $match[0] = preg_replace('/\]\]$/', '', $match[0]);
-
-    $url = preg_replace('/ /', '_', $match[0]);
-    $url = WIKI_BASE_URL . $url;
-    return "<a href=\"$url\">" . $match[0] . "</a>";
-}
